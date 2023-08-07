7c8c6b7e0bb2e019b37e100066563024eff5e1af
ananyabadithe <ananyabpune@gmail.com>
2023-08-07 14:12:32 +0100

    Gradle Sync

--- app/src/main/java/android/kotlin/foodclub/views/home/HomeView.kt
+++ app/src/main/java/android/kotlin/foodclub/views/home/HomeView.kt
@@ -86,6 +86,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import androidx.navigation.NavHostController
 
+
 val montserratFamily = FontFamily(Font(R.font.montserratregular, FontWeight.Normal))
 
 @Composable
