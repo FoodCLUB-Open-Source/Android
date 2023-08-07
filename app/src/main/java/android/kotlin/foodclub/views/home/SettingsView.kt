a9027499da7150df4d6b63bcaa13f611c77671b3
ananyabadithe <ananyabpune@gmail.com>
2023-08-07 22:28:36 +0100

    User Image/Name Section

--- app/src/main/java/android/kotlin/foodclub/views/home/SettingsView.kt
+++ app/src/main/java/android/kotlin/foodclub/views/home/SettingsView.kt
@@ -1,28 +1,22 @@
 package android.kotlin.foodclub.views.home
 
-import android.graphics.drawable.Icon
 import android.kotlin.foodclub.R
-import android.provider.CalendarContract
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.BottomSheetDefaults.ContainerColor
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButtonColors
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -31,12 +25,13 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 val montSerratFamily = FontFamily(Font(R.font.montserratregular, FontWeight.Normal))
-
 //This is the main settings function where everything is being organised
 @Composable
 fun SettingsView(){
     Column {
+
         TopBar()
+        UserContent(userName = "Ananya Badithe", userImage = painterResource(id=R.drawable.app_logo))
     }
 }
 
@@ -44,41 +39,74 @@ fun SettingsView(){
 @Composable
 fun TopBar(){
     Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
+        horizontalArrangement = Arrangement.Start,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top=80.dp),
+            .padding(top = 100.dp),
     ) {
         Column(
-            modifier = Modifier.weight(1f),
+            modifier = Modifier.weight(0.3f),
             horizontalAlignment = Alignment.Start
         ) {
             Button(
                 onClick = { /*Goes to Page before this*/ },
-//                modifier = Modifier.padding(end = 10.dp),
                 colors = ButtonDefaults.buttonColors(containerColor=Color.Transparent, contentColor = Color.Black)
                 ) {
                 Icon(
                     painter = painterResource(id = R.drawable.back_icon),
                     contentDescription = "Back",
-                    
-//                    colors = IconButtonDefaults.iconButtonColors(Color.Black)
+                    modifier = Modifier
+                        .size(25.dp)
                 )
             }
         }
 
         Column(
             modifier = Modifier.weight(1f),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.Start
         ) {
             Text(
                 text = "Settings",
                 fontSize = 28.sp,
                 color = Color.Black,
-                fontFamily = montserratFamily
+                fontFamily = montserratFamily,
+                fontWeight = FontWeight.W900,
+                modifier = Modifier
+                    .padding(top= 3.dp)
 
             )
         }
     }
 }
+
+@Composable
+fun UserContent(userName: String, userImage: Painter){
+    Column(
+
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            Image(
+                contentDescription = "User Images",
+                painter = userImage,
+            )
+
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = userName,
+                fontSize = 24.sp,
+                color = Color.Black,
+                fontFamily = montserratFamily,
+                fontWeight = FontWeight.W900
+            )
+        }
+    }
+}
+
 
