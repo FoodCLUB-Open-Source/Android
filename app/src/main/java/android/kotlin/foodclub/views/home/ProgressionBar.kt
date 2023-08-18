b2714c1c3249f52ce3ee6f2572869fd6792a39a0
ananyabadithe <ananyabpune@gmail.com>
2023-08-18 16:48:12 +0100

    Creation of File

---
+++ app/src/main/java/android/kotlin/foodclub/views/home/ProgressionBar.kt
@@ -0,0 +1,16 @@
+package android.kotlin.foodclub.views.home
+
+import android.provider.MediaStore.Video
+import androidx.compose.runtime.Composable
+
+// This composable is a temporary one to test whether the progression bar works
+@Composable
+fun TestFun(){
+
+}
+
+// Function to Create the progression bar
+@Composable
+fun ProgressionBar(){
+
+}
