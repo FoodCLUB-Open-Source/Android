c76335eb7253785ab40cba1dfa98b230c3c1683a
harv-str <138323318+harv-str@users.noreply.github.com>
2023-08-18 14:53:38 +0000

    shoppingcartFunctionalityMerging

--- app/src/main/java/android/kotlin/foodclub/views/home/MyBasketView.kt
+++ app/src/main/java/android/kotlin/foodclub/views/home/MyBasketView.kt
@@ -1,9 +1,22 @@
 package android.kotlin.foodclub.views.home
 
 import android.kotlin.foodclub.R
+import android.kotlin.foodclub.utils.composables.Picker
+import android.kotlin.foodclub.utils.composables.rememberPickerState
+import android.kotlin.foodclub.viewmodels.home.CreateRecipeViewModel
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.with
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,35 +30,86 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.TextField
+//import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntSize
+import java.util.Collections.copy
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyBasketView() {
+
+    val systemUiController = rememberSystemUiController()
+    var showSheet by remember { mutableStateOf(false) }
+
+    val triggerBottomSheetModal: () -> Unit = {
+        showSheet = !showSheet
+        systemUiController.setStatusBarColor(
+            color = if (showSheet) Color(android.graphics.Color.parseColor("#ACACAC")) else Color.White,
+            darkIcons = true
+        )
+        systemUiController.setNavigationBarColor(
+            color = if (showSheet) Color.Black else Color.White,
+            darkIcons = true
+        )
+    }
+
+    if (showSheet) {
+        BottomSheetIngredients(triggerBottomSheetModal)
+    }
     Column(
         modifier = Modifier
             .background(color = Color(0xFFF0F0F0))//Remove when doing navigation
@@ -125,7 +189,9 @@ fun MyBasketView() {
                         containerColor = Color.White,
                         contentColor = Color(126, 198, 11, 255)
                     ), contentPadding = PaddingValues(15.dp),
-                    onClick = {}
+                    onClick = {
+                        triggerBottomSheetModal()
+                    }
                 ) {
                     Text(
                         "Add items +",
@@ -242,10 +308,8 @@ fun MyBasketView() {
 
 
 
-@Composable
-@Preview
-fun PreviewMyBasketView() {
-    MyBasketView()
-}
+
+
+
 
 
