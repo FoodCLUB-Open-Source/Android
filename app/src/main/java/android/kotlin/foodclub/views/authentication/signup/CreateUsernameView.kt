91c251ccb59e37567fef0a6a9f1f0fa734761ad5
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2023-09-13 13:09:39 +0200

    Create new package to contain all views involving sign up process

--- app/src/main/java/android/kotlin/foodclub/views/authentication/signup/CreateUsernameView.kt
+++ app/src/main/java/android/kotlin/foodclub/views/authentication/signup/CreateUsernameView.kt
@@ -1,4 +1,4 @@
-package android.kotlin.foodclub.views.authentication
+package android.kotlin.foodclub.views.authentication.signup
 
 import android.kotlin.foodclub.R
 import android.kotlin.foodclub.api.authentication.UserSignUpInformation
@@ -16,9 +16,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -34,7 +31,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
