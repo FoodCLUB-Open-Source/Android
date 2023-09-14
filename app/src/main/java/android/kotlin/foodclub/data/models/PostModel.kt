147f8b18264ebfbe187f5937f31cb32536a6530b
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2023-09-14 22:14:03 +0200

    Create appropriate models to fetch api call response data.

---
+++ app/src/main/java/android/kotlin/foodclub/data/models/PostModel.kt
@@ -0,0 +1,22 @@
+package android.kotlin.foodclub.data.models
+
+import com.google.gson.annotations.SerializedName
+
+data class PostModel(
+    val id: Long,
+    val title: String,
+    val description: String,
+    val username: String,
+
+    @SerializedName("profile_picture")
+    val profilePictureUrl: String?,
+    @SerializedName("video_url")
+    val videoUrl: String,
+    @SerializedName("thumbnail_url")
+    val thumbnailUrl: String,
+
+    @SerializedName("total_likes")
+    val likes: Long,
+    @SerializedName("total_views")
+    val views: Long
+)
