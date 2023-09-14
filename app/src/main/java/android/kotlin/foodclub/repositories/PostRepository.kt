573c6abebbcfd326c84dd49e25d6105ccbc69c96
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2023-09-14 22:14:51 +0200

    Create repository to map Data transfer object (Dto) to VideoModel, get post and delete post.

---
+++ app/src/main/java/android/kotlin/foodclub/repositories/PostRepository.kt
@@ -0,0 +1,61 @@
+package android.kotlin.foodclub.repositories
+
+import android.kotlin.foodclub.api.authentication.API
+import android.kotlin.foodclub.data.models.PostModel
+import android.kotlin.foodclub.data.models.VideoModel
+import android.kotlin.foodclub.utils.helpers.Resource
+import java.io.IOException
+
+class PostRepository(
+    private val api: API
+) {
+    private fun mapDtoToModel(dtoModel: PostModel): VideoModel {
+        return VideoModel(
+            videoId = dtoModel.id,
+            authorDetails = dtoModel.username,
+            videoStats = VideoModel.VideoStats(
+                dtoModel.likes,
+                0L,
+                0L,
+                0L,
+                dtoModel.views
+            ),
+            videoLink = dtoModel.videoUrl,
+            description = dtoModel.description,
+            thumbnailLink = dtoModel.thumbnailUrl
+        )
+    }
+    suspend fun getPost(id: Long): Resource<VideoModel> {
+        val response = try {
+            api.getPost(id)
+        } catch (e: IOException) {
+            return Resource.Error("Cannot retrieve data. Check your internet connection and try again.")
+        } catch (e: Exception) {
+            return Resource.Error("Unknown error occurred.")
+        }
+
+        if(response.isSuccessful && response.body() != null && response.body()?.data != null
+            && response.body()?.data?.isEmpty() == false){
+            return Resource.Success(mapDtoToModel(response.body()!!.data[0]))
+        }
+        return Resource.Error("Unknown error occurred.")
+
+    }
+
+    suspend fun deletePost(id: Long): Resource<Boolean> {
+        val response = try {
+            api.deletePost(id)
+        } catch (e: IOException) {
+            return Resource.Error("Cannot retrieve data. Check your internet connection and try again.")
+        } catch (e: Exception) {
+            return Resource.Error("Unknown error occurred.")
+        }
+
+        if(response.isSuccessful && response.body() != null && response.body()?.status == "Post Deleted") {
+            return Resource.Success(true)
+        }
+        return Resource.Error("Unknown error occurred.")
+
+    }
+
+}
