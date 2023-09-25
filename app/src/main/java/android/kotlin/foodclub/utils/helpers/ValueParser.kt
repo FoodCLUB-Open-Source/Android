08479841a45c112527d3dea40dc072feac796bc2
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2023-09-25 22:03:19 +0100

    Add common error response parser so that common errors are displayed as String.

--- app/src/main/java/android/kotlin/foodclub/utils/helpers/ValueParser.kt
+++ app/src/main/java/android/kotlin/foodclub/utils/helpers/ValueParser.kt
@@ -1,5 +1,11 @@
 package android.kotlin.foodclub.utils.helpers
 
+import android.kotlin.foodclub.api.authentication.SignUpResponseMessage
+import android.kotlin.foodclub.api.responses.DefaultErrorResponse
+import android.util.Log
+import com.google.gson.Gson
+import retrofit2.Response
+
 class ValueParser {
     companion object {
         fun numberToThousands(number: Long): String {
@@ -7,6 +13,20 @@ class ValueParser {
                 return number.toString()
             }
             return String.format("%.1f",number.toDouble()/1000) + "K"
+        }
+
+        fun <T : Any> errorResponseToMessage(response: Response<T>): String {
+            if(response.errorBody() == null) return "Unknown error occurred."
+
+            val errorResponse = Gson().fromJson(response.errorBody()?.string(), DefaultErrorResponse::class.java)
+
+            return if(errorResponse.errors.isNotEmpty()) {
+                "Input data are invalid. Check mistakes and try again."
+            } else if(errorResponse.message.isNotEmpty()) {
+                errorResponse.message
+            } else {
+                "Unknown error occurred."
+            }
         }
     }
 }
