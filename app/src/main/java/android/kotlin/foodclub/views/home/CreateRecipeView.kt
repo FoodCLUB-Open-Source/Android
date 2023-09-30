353ae6dba504136eacf6e823ec6e94b338f3467b
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2023-09-30 01:42:50 +0100

    Move CreateRecipeBottomSheetIngredients and used composables to new file IngredientsBottomSheet.kt and refactored code.

--- app/src/main/java/android/kotlin/foodclub/views/home/CreateRecipeView.kt
+++ app/src/main/java/android/kotlin/foodclub/views/home/CreateRecipeView.kt
@@ -4,26 +4,17 @@ import android.annotation.SuppressLint
 import android.kotlin.foodclub.R
 import android.kotlin.foodclub.data.models.Ingredient
 import android.kotlin.foodclub.data.models.Recipe
-import android.kotlin.foodclub.utils.composables.Picker
-import android.kotlin.foodclub.utils.composables.rememberPickerState
-import android.kotlin.foodclub.utils.enums.QuantityUnit
+import android.kotlin.foodclub.utils.composables.IngredientsBottomSheet
 import android.kotlin.foodclub.utils.helpers.ValueParser
 import android.kotlin.foodclub.viewmodels.home.CreateRecipeViewModel
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.SizeTransform
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.AnimationSpec
 import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkOut
-import androidx.compose.animation.with
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -73,7 +64,6 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.BottomSheetDefaults
@@ -95,11 +85,9 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntSize
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
@@ -129,10 +117,6 @@ data class Colors(
 
 val montserratFamily = FontFamily(Font(R.font.montserratregular, FontWeight.Normal))
 
-enum class DrawerContentState {
-    IngredientListContent,
-    IngredientAmountSelection
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -269,248 +253,6 @@ fun BottomSheetCategories(onDismiss: () -> Unit) {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
-@Composable
-fun CreateRecipeBottomSheetIngredients(onDismiss: () -> Unit, onSave: (ingredient: Ingredient) -> Unit = {}) {
-    val ingredientList = listOf(
-        Ingredient(1, "Olive Oil", 0, QuantityUnit.MILLILITERS, "https://kretu.sts3.pl/foodclub_drawable/tomato_ingredient.png"),
-        Ingredient(2, "Tomato", 120, QuantityUnit.GRAMS, "https://kretu.sts3.pl/foodclub_drawable/tomato_ingredient.png"),
-        Ingredient(3, "Lettuce", 50, QuantityUnit.GRAMS, "https://kretu.sts3.pl/foodclub_drawable/salad_ingredient.png"),
-        Ingredient(4, "Broccoli", 0, QuantityUnit.GRAMS, "https://kretu.sts3.pl/foodclub_drawable/salad_ingredient.png"),
-        Ingredient(5, "Carrot", 0, QuantityUnit.GRAMS, "https://kretu.sts3.pl/foodclub_drawable/salad_ingredient.png"),
-        Ingredient(6, "Flour", 0, QuantityUnit.GRAMS, "https://kretu.sts3.pl/foodclub_drawable/tomato_ingredient.png"),
-        Ingredient(7, "Milk", 0, QuantityUnit.MILLILITERS, "https://kretu.sts3.pl/foodclub_drawable/tomato_ingredient.png")
-    )
-    var editedIngredient by remember { mutableStateOf(ingredientList[0]) }
-
-    val coroutineScope = rememberCoroutineScope()
-    val screenHeight = LocalConfiguration.current.screenHeightDp.dp - 150.dp
-    var searchText by remember { mutableStateOf("") }
-    val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
-    val contentState = remember { mutableStateOf(DrawerContentState.IngredientListContent) }
-    val values = remember { mutableStateOf((1..99).map {
-        (it * 10).toString() + ValueParser.quantityUnitToString(editedIngredient.unit) })
-    }
-    val valuesPickerState = rememberPickerState()
-
-    ModalBottomSheet(
-        containerColor = Color.Black,
-        onDismissRequest = { onDismiss() },
-        sheetState = bottomSheetState,
-        dragHandle = { BottomSheetDefaults.DragHandle() },
-    ) {
-        AnimatedContent(targetState = contentState, transitionSpec = {
-            fadeIn(animationSpec = tween(150, 150)) with
-                    fadeOut(animationSpec = tween(150)) using
-                    SizeTransform { initialSize, targetSize ->
-                        if (targetState.value == DrawerContentState.IngredientListContent) {
-                            keyframes {
-                                IntSize(targetSize.width, initialSize.height) at 150
-                                durationMillis = 300
-                            }
-                        } else {
-                            keyframes {
-                                IntSize(initialSize.width, targetSize.height) at 150
-                                durationMillis = 300
-                            }
-                        }
-                    }
-
-        }, label = ""
-        ) { contentState ->
-            when (contentState.value) {
-                DrawerContentState.IngredientListContent -> {
-                    Box( modifier = Modifier
-                        .fillMaxWidth()
-                        .height(screenHeight) ) {
-                        Box(modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 17.dp, end = 17.dp),
-                            contentAlignment = Alignment.CenterStart) {
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically,
-                                horizontalArrangement = Arrangement.Start,
-                                modifier = Modifier.clickable {
-                                    coroutineScope.launch {
-                                        bottomSheetState.hide()
-                                        onDismiss()
-                                    }
-                                }
-                            ) {
-                                Image(
-                                    painter = painterResource(id = R.drawable.baseline_arrow_back_ios_new_24),
-                                    contentDescription = "Left Arrow",
-                                    modifier = Modifier.size(24.dp)
-                                )
-                                Spacer(modifier = Modifier.width(8.dp))
-                                Text(text = "Add items", color = Color.White, fontFamily = montserratFamily)
-                            }
-                        }
-                        LazyColumn( modifier = Modifier.padding(top = 30.dp)) {
-                            item {
-                                Row(
-                                    verticalAlignment = Alignment.CenterVertically,
-                                    horizontalArrangement = Arrangement.SpaceBetween,
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(20.dp)
-                                ) {
-                                    TextField(
-                                        value = searchText,
-                                        onValueChange = { searchText = it },
-                                        placeholder = {
-                                            Text(text = "Search here", fontSize = 15.sp)
-                                        },
-                                        shape = RoundedCornerShape(12.dp),
-                                        singleLine = true,
-                                        keyboardOptions = KeyboardOptions(
-                                            imeAction = ImeAction.Done
-                                        ),
-                                        keyboardActions = KeyboardActions(
-                                            onDone = { /* Handle onDone event if needed */ }
-                                        ),
-                                        colors = TextFieldDefaults.textFieldColors(
-                                            containerColor = Color.White,
-                                            cursorColor = Color.Black,
-                                            focusedIndicatorColor = Color.Transparent,
-                                            unfocusedIndicatorColor = Color.Transparent
-                                        ),
-                                        modifier = Modifier
-                                            .weight(1f)
-                                            .height(51.dp)
-                                    )
-                                    Spacer(modifier = Modifier.width(20.dp))
-                                    Text(
-                                        text = "Clear",
-                                        color = Color(android.graphics.Color.parseColor("#545454")),
-                                        modifier = Modifier.clickable { searchText = "" }
-                                    )
-                                    Spacer(modifier = Modifier.width(20.dp))
-                                }
-                            }
-                            items(ingredientList.size) {
-                                AddItemComposable(ingredientList[it], onClick = { ingredient ->
-                                    values.value = (1..99).map { value ->
-                                        (value * 10).toString() +
-                                                ValueParser.quantityUnitToString(ingredient.unit)
-                                    }
-                                    editedIngredient = ingredient
-                                    contentState.value = DrawerContentState.IngredientAmountSelection
-                                })
-                            }
-                        }
-                    }
-                }
-                DrawerContentState.IngredientAmountSelection -> {
-                    Box( modifier = Modifier
-                        .fillMaxWidth()
-                        .height(screenHeight) ) {
-                        Column(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(start = 17.dp, end = 17.dp),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.SpaceBetween,
-                        ) {
-                            Box(
-                                modifier = Modifier
-                                    .height(50.dp)
-                                    .fillMaxWidth()
-                            ) {
-                                Image(
-                                    painter = painterResource(id = R.drawable.baseline_arrow_back_ios_new_24),
-                                    contentDescription = "Left Arrow",
-                                    modifier = Modifier
-                                        .size(24.dp)
-                                        .align(Alignment.CenterStart)
-                                        .clickable(onClick = {
-                                            contentState.value =
-                                                DrawerContentState.IngredientListContent
-                                        })
-                                )
-                                Spacer(modifier = Modifier.width(8.dp))
-                                Text(
-                                    text = "Add items",
-                                    color = Color.White,
-                                    fontFamily = montserratFamily,
-                                    modifier = Modifier.align(Alignment.Center)
-                                )
-                            }
-                                Column(
-                                    horizontalAlignment = Alignment.CenterHorizontally,
-                                    verticalArrangement = Arrangement.SpaceBetween,
-                                    modifier = Modifier
-                                        .fillMaxSize()
-                                        .padding(top = 30.dp, bottom = 40.dp)
-                                ) {
-                                    AsyncImage(
-                                        model = editedIngredient.imageUrl,
-                                        contentDescription = null,
-                                        modifier = Modifier
-                                            .size(130.dp)
-                                            .clip(CircleShape)
-                                    )
-//                                    Image(
-//                                        painter = painterResource(id = R.drawable.tomato_ingredient),
-//                                        contentDescription = "Circular Image",
-//                                        modifier = Modifier
-//                                            .size(130.dp)
-//                                            .clip(CircleShape)
-//                                    )
-                                    Spacer(modifier = Modifier.height(35.dp))
-                                    Picker(
-                                        state = valuesPickerState,
-                                        items = values.value,
-                                        visibleItemsCount = 3,
-                                        modifier = Modifier.weight(0.3f),
-                                        textModifier = Modifier.padding(8.dp),
-                                        textStyle = TextStyle(fontSize = 20.sp, color = Color(android.graphics.Color.parseColor("#545454")))
-                                    )
-                                    Button(
-                                        shape = RectangleShape,
-                                        modifier = Modifier
-                                            .border(
-                                                1.dp,
-                                                Color(126, 198, 11, 255),
-                                                shape = RoundedCornerShape(15.dp)
-                                            )
-                                            .clip(RoundedCornerShape(15.dp))
-                                            .fillMaxWidth(),
-                                        colors = ButtonDefaults.buttonColors(
-                                            containerColor = Color(126, 198, 11, 255),
-                                            contentColor = Color.White
-                                        ), contentPadding = PaddingValues(15.dp),
-                                        onClick = {
-                                            onSave(
-                                                Ingredient(
-                                                    editedIngredient.id,
-                                                    editedIngredient.type,
-                                                    ValueParser.quantityStringToInt(
-                                                        valuesPickerState.selectedItem,
-                                                        editedIngredient.unit
-                                                    ),
-                                                    editedIngredient.unit,
-                                                    editedIngredient.imageUrl
-                                                )
-                                            )
-                                            coroutineScope.launch {
-                                                bottomSheetState.hide()
-                                                onDismiss()
-                                            }
-                                        }
-                                    ) {
-                                        Text("Save", color = Color.White, fontFamily = montserratFamily, fontSize = 16.sp, fontWeight = FontWeight.ExtraBold)
-                                    }
-                                }
-                            }
-
-                    }
-                }
-            }
-        }
-    }
-}
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateRecipeView(navController: NavController) {
@@ -570,7 +312,7 @@ fun CreateRecipeView(navController: NavController) {
             .padding(start = 15.dp, top = 0.dp, end = 15.dp, bottom = 70.dp)
     ) {
         if (showSheet) {
-            CreateRecipeBottomSheetIngredients(triggerBottomSheetModal)
+            IngredientsBottomSheet(triggerBottomSheetModal)
         }
         if (showCategorySheet) {
             BottomSheetCategories(triggerCategoryBottomSheetModal)
@@ -967,40 +709,6 @@ fun Ingredient(ingredient: Ingredient, isRevealed: Boolean, onExpand: () -> Unit
     }
 }
 
-@Composable
-fun AddItemComposable(
-    ingredient: Ingredient,
-    onClick: (Ingredient) -> Unit
-) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(70.dp)
-            .padding(start = 20.dp, top = 20.dp)
-            .clickable(onClick = { onClick(ingredient) })
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start
-        ) {
-            AsyncImage(
-                model = ingredient.imageUrl,
-                contentDescription = null,
-                modifier = Modifier
-                    .size(40.dp)
-                    .clip(CircleShape)
-            )
-            Spacer(modifier = Modifier.width(15.dp))
-            Text(
-                text = ingredient.type,
-                color = Color.White,
-                fontFamily = montserratFamily
-            )
-        }
-    }
-}
 @Composable
 fun SectionItem(
     title: String,
