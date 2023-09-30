b091d09506aaa30760f8f7e803daa87109ebbf64
shubhamjain207 <shubhamjain9708@gmail.com>
2023-09-30 03:41:38 +0100

    ForgotPasswordBug

--- app/src/main/java/android/kotlin/foodclub/viewmodels/authentication/EmailSentViewModel.kt
+++ app/src/main/java/android/kotlin/foodclub/viewmodels/authentication/EmailSentViewModel.kt
@@ -1,6 +1,25 @@
 package android.kotlin.foodclub.viewmodels.authentication
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavHostController
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class EmailSentViewModel : ViewModel() {
+
+
+    fun goToLogin(navController: NavHostController){
+        viewModelScope.launch(Dispatchers.Main) {
+
+            try{
+                navController.navigate("LOGIN")
+            }catch (e:Exception){
+
+            }
+
+
+        }
+
+    }
 }
