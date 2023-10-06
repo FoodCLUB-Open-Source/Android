7361d1351c4cc1889fd0dfbc156ad9da77be26fc
acb21jk <jkreczetowski1@sheffield.ac.uk>
2023-10-06 15:44:38 +0100

    Remove repeated products ids in ProductsData

--- app/src/main/java/android/kotlin/foodclub/data/models/ProductsData.kt
+++ app/src/main/java/android/kotlin/foodclub/data/models/ProductsData.kt
@@ -1,5 +1,19 @@
 package android.kotlin.foodclub.data.models
 
-class ProductsData(val searchText: String, val productsList: List<Ingredient>) {
+class ProductsData(val searchText: String, productsList: List<Ingredient>) {
+    var productsList: List<Ingredient> = productsList
+        private set
+
+    init {
+        removeRepeatedIds()
+    }
+
+    private fun removeRepeatedIds() {
+        val productsMap: MutableMap<String, Ingredient> = mutableMapOf()
+        productsList.forEach {
+            if (!productsMap.containsKey(it.id)) productsMap.put(it.id, it)
+        }
+        productsList = productsMap.map { it.value }
+    }
     
 }
