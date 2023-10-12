5d7b96f6b46994d85ea694a71ef4a74494011d26
acb21jk <jkreczetowski1@sheffield.ac.uk>
2023-10-12 17:25:23 +0100

    Modify Session to take token and create SessionUser object from it

--- app/src/main/java/android/kotlin/foodclub/data/models/Session.kt
+++ app/src/main/java/android/kotlin/foodclub/data/models/Session.kt
@@ -1,5 +1,19 @@
 package android.kotlin.foodclub.data.models
 
-data class Session(
-    val userId: Long
-)
+import com.auth0.android.jwt.JWT
+
+class Session(val token: String) {
+    var sessionUser: SessionUser? = null
+        private set
+
+    init {
+        decodeToken()
+    }
+
+    private fun decodeToken() {
+        val jwt = JWT(token)
+        val userId = jwt.getClaim("userId")
+        val expiryAt = jwt.getClaim("expiryAt")
+        sessionUser = SessionUser(userId.asLong() ?: 0, expiryAt.asLong() ?: 0)
+    }
+}
