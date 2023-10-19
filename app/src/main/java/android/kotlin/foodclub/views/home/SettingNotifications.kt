efa2e5ee51a6a5dad385788e83fa96e3e62fe6ad
Karennnz <s2214450@ed.ac.uk>
2023-10-19 18:20:13 +0100

    Update and finished Notification

--- app/src/main/java/android/kotlin/foodclub/views/home/SettingNotifications.kt
+++ app/src/main/java/android/kotlin/foodclub/views/home/SettingNotifications.kt
@@ -1,5 +1,6 @@
 package android.kotlin.foodclub.views.home
 
+import android.kotlin.foodclub.utils.composables.SettingsLayout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -22,6 +23,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
@@ -35,100 +37,70 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
-@Composable
-fun BlackArrow() {
-    //need to fix the arrow thing
-    Box(
-        modifier = Modifier
-            .size(24.dp)
-            .background(color = Color.Gray, shape = RoundedCornerShape(4.dp))
-            .padding(8.dp)
-    ) {
-        Icon(
-            imageVector = Icons.Default.ArrowBack, // Arrow pointing to the left
-            contentDescription = null,
-            tint = Color.Black
-        )
-    }
-}
 
 @Composable
 fun SettingNotifications(navController: NavController) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            BlackArrow()
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(
-                text = "Notifications",
-                style = MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.Bold),
-            )
-        }
-        Spacer(modifier = Modifier.height(32.dp))
-        NotificationRow("General notifications")
-        Text(
-            text = "System & Service update",
-            style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
-            modifier = Modifier.padding(start = 4.dp)
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-
+    SettingsLayout("Notifications", onBackAction = { navController.navigateUp() }) {
+        NotificationRow("General notifications", titlePadding = 4.dp)
+        NotificationRow("System & Service update", isSubtitle = true)
         NotificationRow("App updates")
         NotificationRow("Payment request")
     }
 }
 
-
-
-
 @Composable
-fun NotificationRow(title: String){
+fun NotificationRow(title: String, isSubtitle: Boolean = false, titlePadding: Dp = 8.dp) {
     var isNotificationEnabled by remember {
         mutableStateOf(false)
     }
 
-    Row(
+    val verticalPadding = if (isSubtitle) 4.dp else 8.dp
+
+    Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp),
-    ){
-        Text(
-            text = title,
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier
-                .weight(1f)
-        )
-        //change the color of the switch button to light green
-
-        Switch(
-            checked = isNotificationEnabled,
-            onCheckedChange = {
-                isNotificationEnabled = it
-            },
-            //the color should be more lighter green, when it is turned on and the thumb should be white
-            colors = SwitchDefaults.colors(
-                checkedThumbColor = Color.White,
-               // checkedTrackColor = Color(0xFF4CAF50),
-                checkedTrackColor = Color(0xFF4CAF50),
-                uncheckedThumbColor = Color.White,
-                uncheckedTrackColor = Color(0xFFBDBDBD),
-            ),
+            .padding(vertical = verticalPadding)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.bodyMedium.copy(
+                    fontWeight = if (isSubtitle) FontWeight.Bold else FontWeight.Normal
+                ),
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = titlePadding)
             )
+
+            if (!isSubtitle) {
+                Switch(
+                    checked = isNotificationEnabled,
+                    onCheckedChange = {
+                        isNotificationEnabled = it
+                    },
+                    colors = SwitchDefaults.colors(
+                        checkedThumbColor = Color.White,
+                        checkedTrackColor = Color(0xFF7EC60B),
+                        uncheckedThumbColor = Color.White,
+                        uncheckedTrackColor = Color(0xFFBDBDBD),
+                    )
+                )
+            }
+        }
     }
 }
 
