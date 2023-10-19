8a148d68ecc7060ab7ed485520d96628172b6f15
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2023-10-19 21:51:48 +0100

    Remove ForgotPasswordView from onBoardingNavigationGraph

--- app/src/main/java/android/kotlin/foodclub/navigation/graphs/OnBoardingNavigationGraph.kt
+++ app/src/main/java/android/kotlin/foodclub/navigation/graphs/OnBoardingNavigationGraph.kt
@@ -4,7 +4,6 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
-import android.kotlin.foodclub.views.authentication.ForgotPasswordView
 
 fun NavGraphBuilder.onBoardingNavigationGraph(navController: NavHostController,
                                               setBottomBarVisibility: (Boolean) -> Unit) {
@@ -13,7 +12,7 @@ fun NavGraphBuilder.onBoardingNavigationGraph(navController: NavHostController,
         startDestination = OnBoardingScreen.Menu.route
     ) {
         composable(route = OnBoardingScreen.Menu.route) {
-            ForgotPasswordView(navController)
+
         }
     }
 }
