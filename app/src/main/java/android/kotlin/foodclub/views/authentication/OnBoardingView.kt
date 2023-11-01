13834021dfa98dfe5e16b450863799da97b7d622
Eric Leon <ericleon727@gmail.com>
2023-11-01 16:04:38 +0000

    Add new screens to onboarding need to change indicators

--- app/src/main/java/android/kotlin/foodclub/views/authentication/OnBoardingView.kt
+++ app/src/main/java/android/kotlin/foodclub/views/authentication/OnBoardingView.kt
@@ -2,8 +2,10 @@ package android.kotlin.foodclub.views.authentication
 
 import android.kotlin.foodclub.R
 import android.kotlin.foodclub.config.ui.Montserrat
+import android.kotlin.foodclub.config.ui.defaultButtonColors
 import android.kotlin.foodclub.config.ui.foodClubGreen
 import android.kotlin.foodclub.domain.models.others.OnBoardingItems
+import android.kotlin.foodclub.views.home.montserratFamily
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -17,8 +19,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
@@ -30,6 +37,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
@@ -48,22 +56,143 @@ fun OnBoardingView(){
 
     Column(modifier = modifier.fillMaxSize()) {
         HorizontalPager(
-            count = items.size,
+            count = 4,
             state = pageState,
             modifier = modifier
                 .fillMaxHeight(0.9f)
                 .fillMaxWidth()
         ) { page ->
-            OnBoardingItem(modifier = modifier, item = items[page])
+            //OnBoardingItem(modifier = modifier, item = items[page])
+            if(page == 0)
+            {
+                OnBoardingScreen1()
+            }
+            if(page == 1)
+            {
+                OnBoardingScreen2()
+            }
+            if(page == 2)
+            {
+                OnBoardingScreen3()
+            }
+            if(page == 3)
+            {
+                OnBoardingScreen4()
+            }
+
         }
         BottomSection(
-            size = items.size,
+            size = 4,
             index = pageState.currentPage,
         ) {
             if (pageState.currentPage + 1 < items.size) scope.launch {
                 pageState.scrollToPage(pageState.currentPage + 1)
             }
         }
+    }
+}
+
+@Composable
+fun OnBoardingScreen1()
+{
+    Column(
+        Modifier
+            .padding(10.dp)
+            .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center)
+    {
+        Image(painter = painterResource(id = R.drawable.app_logo), contentDescription = null, contentScale = ContentScale.Crop, modifier = Modifier.size(200.dp))
+        Text(text = "FoodCLUB", fontFamily = montserratFamily, fontWeight = FontWeight.Black, fontSize = 35.sp)
+        Text(text="The Art of Food is Here", fontFamily = montserratFamily, fontSize = 20.sp, color = Color.Gray)
+    }
+}
+
+@Composable
+fun OnBoardingScreen2()
+{
+    Column(
+        Modifier
+            .padding(10.dp)
+            .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center)
+    {
+        Text(text = "Don't follow,", color = Color(0xFF3A7CA8), fontSize = 45.sp, fontWeight = FontWeight.ExtraBold)
+        Text(text = "create.", color = foodClubGreen, fontSize = 80.sp, fontWeight = FontWeight.ExtraBold)
+    }
+}
+
+@Composable
+fun OnBoardingScreen3()
+{
+    Column(
+        Modifier
+            .padding(30.dp)
+            .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceEvenly){
+        Text(fontFamily = montserratFamily,
+            fontWeight = FontWeight.ExtraBold,
+            text="Our Mission",
+            fontSize = 40.sp)
+        Text(fontFamily = montserratFamily,
+            fontWeight = FontWeight.Bold,
+            text="Did you know?",
+            fontSize = 27.5.sp,
+            modifier = Modifier.align(Alignment.Start))
+
+        InfoRow(imageID = R.drawable.app_logo, text = "25% of climate change and 70% of fresh water is linked to food production")
+        InfoRow(imageID = R.drawable.app_logo, text = "70% of disease and illness are caused by nutrition")
+        InfoRow(imageID = R.drawable.app_logo, text = "We produce food across the glob to wipe out global hunger - but 40% of that food goes to waste")
+        InfoRow(imageID = R.drawable.app_logo, text = "1 in 10 people have a food disorder")
+        InfoRow(imageID = R.drawable.app_logo, text = "Social media has left us unconnected in our communities and our cultures")
+
+        Text(fontFamily = montserratFamily,
+            fontWeight = FontWeight.Bold,
+            text="You're now part of our mission.",
+            fontSize = 21.5.sp,
+            modifier = Modifier.align(Alignment.Start))
+    }
+}
+
+@Composable
+fun OnBoardingScreen4()
+{
+    Column(
+        Modifier
+            .padding(30.dp)
+            .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceEvenly){
+        Text(fontFamily = montserratFamily,
+            fontWeight = FontWeight.Bold,
+            text="Join Our Discord VIP Community",
+            fontSize = 40.sp)
+
+        InfoRow(imageID = R.drawable.app_logo, text = "Join our community for exclusive content and insight into the tech and business world")
+        Divider()
+        InfoRow(imageID = R.drawable.app_logo, text = "Engage in lively discussions and events.")
+        Divider()
+        InfoRow(imageID = R.drawable.app_logo, text = "Shape the future of food: help decide new features and innovation.")
+        Divider()
+        InfoRow(imageID = R.drawable.app_logo, text = "Participate in special contests and giveaways.")
+        Divider()
+        InfoRow(imageID = R.drawable.app_logo, text = "Be the first to savor insights into foodSNAPS, foodMAPS, foodSCHOOL, and our detectable AI-driven innovations.")
+        
+        Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF3A7CA8)), shape = RoundedCornerShape(10.dp)) {
+            Text(text = "Join the VIP Discord Server", fontFamily = montserratFamily, color = Color.White, fontSize = 24.sp)
+        }
+    
+    }
+}
+
+@Composable
+fun InfoRow(imageID: Int, text: String)
+{
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+        .fillMaxWidth()
+        .padding(5.dp))
+    {
+        Image(painter = painterResource(id = imageID),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(55.dp))
+        Text(text = text, fontFamily = montserratFamily, modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp), color = Color.Gray, fontSize = 16.sp)
     }
 }
 
@@ -164,8 +293,28 @@ fun BoxScope.Indicators(size: Int, index: Int) {
         horizontalArrangement = Arrangement.spacedBy(30.dp),
         modifier = Modifier.align(Alignment.BottomCenter)
     ) {
+        if(index != 0)
+        {
+            Button(onClick = { /*TODO*/ }, colors = defaultButtonColors(containerColor = Color.Transparent)) {
+                Image(painter = painterResource(id = R.drawable.back_icon), contentDescription = null, modifier = Modifier.size(50.dp))
+            }
+        }
         repeat(size) {
             Indicator(isSelected = it == index)
         }
+
+        if (index != size-1)
+        {
+            Button(onClick = { /*TODO*/ }, colors = defaultButtonColors(containerColor = Color.Transparent)) {
+                Image(painter = painterResource(id = R.drawable.baseline_arrow_forward_ios_24), contentDescription = null, modifier = Modifier.size(50.dp))
+            }
+        }
     }
+}
+
+@Composable
+@Preview
+fun OnBoardingPreview()
+{
+    OnBoardingView()
 }
