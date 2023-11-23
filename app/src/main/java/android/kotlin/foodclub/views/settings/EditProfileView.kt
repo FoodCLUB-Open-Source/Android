93e1b004e9e96a49c381853b5f5205314ecaf8d9
Elliot Thompson <elliotthompson101@gmail.com>
2023-11-23 14:30:04 -0800

    add settings ui views strings to resource file

--- app/src/main/java/android/kotlin/foodclub/views/settings/EditProfileView.kt
+++ app/src/main/java/android/kotlin/foodclub/views/settings/EditProfileView.kt
@@ -1,5 +1,6 @@
 package android.kotlin.foodclub.views.settings
 
+import android.kotlin.foodclub.R
 import android.kotlin.foodclub.domain.models.profile.UserDetailsModel
 import android.kotlin.foodclub.utils.composables.ConfirmButton
 import android.kotlin.foodclub.utils.composables.CustomTextField
@@ -14,27 +15,43 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
-//The main function of this EditProfile file. This arranges all components to build the screen
 @Composable
 fun EditProfileSetting(
     navController: NavController,
     userState: State<UserDetailsModel?>,
     viewModel: SettingsViewModel
-    ){
+) {
     val user = userState.value
 
-    SettingsLayout(label = "Edit Profile", onBackAction = { navController.navigateUp()}) {
+    SettingsLayout(
+        label = stringResource(id = R.string.edit_profile),
+        onBackAction = { navController.navigateUp() }) {
         var username by remember { mutableStateOf("") }
         var email by remember { mutableStateOf("") }
-        CustomTextField(placeholder = "Username", keyboardType = KeyboardType.Text, onValueChange ={ username= it })
+
+        CustomTextField(
+            placeholder = stringResource(id = R.string.username),
+            keyboardType = KeyboardType.Text,
+            onValueChange = { username = it })
+
         Spacer(modifier = Modifier.height(12.dp))
-        CustomTextField(placeholder = "Email", keyboardType = KeyboardType.Text, onValueChange ={ email= it })
+
+        CustomTextField(
+            placeholder = stringResource(id = R.string.email),
+            keyboardType = KeyboardType.Text,
+            onValueChange = { email = it })
+
         Spacer(modifier = Modifier.height(16.dp))
-        ConfirmButton(enabled = true, text = "Save") {
+
+        ConfirmButton(
+            enabled = true,
+            text = stringResource(id = R.string.save)
+        ) {
             val testUser = user!!.copy(
                 phoneNumber = "07123931923"
             )
