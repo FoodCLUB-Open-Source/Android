c6645d8cb0c309d3fc6274dffc80f9c7685fb866
Kretu <kret1315@gmail.com>
2023-11-28 18:52:16 +0000

    Revert "Ingredient api task"

--- app/src/main/java/android/kotlin/foodclub/views/home/MyBasketView.kt
+++ app/src/main/java/android/kotlin/foodclub/views/home/MyBasketView.kt
@@ -26,7 +26,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -50,6 +49,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
@@ -61,19 +61,11 @@ fun MyBasketView() {
     val viewModel: MyBasketViewModel = hiltViewModel()
     val systemUiController = rememberSystemUiController()
     var showSheet by remember { mutableStateOf(false) }
-    val productsList by viewModel.productsList.collectAsState()
-    val selectedProductsIds by viewModel.selectedProductsList.collectAsState()
-    val selectedIngredients by viewModel.selectedIngredients.collectAsState()
+    val productsList = viewModel.productsList.collectAsState()
+    val selectedProductsIds = viewModel.selectedProductsList.collectAsState()
     var deleteSelected by remember { mutableStateOf(false) }
 
     val triggerBottomSheetModal: () -> Unit = {
-        // GETTING SELECTED INGREDIENT FROM HOME VIEW MODEL
-        val selectedIngredients = viewModel.selectedIngredients.value
-
-        // PASSING SELECTED INGREDIENT TO MY BASKET VIEW
-        viewModel.addIngredientsToBasket(selectedIngredients)
-        viewModel.updateSelectedIngredients(emptyList())
-
         showSheet = !showSheet
         systemUiController.setStatusBarColor(color = Color(0x00ACACAC), darkIcons = true)
         systemUiController.setNavigationBarColor(color = Color.Black, darkIcons = true)
@@ -92,62 +84,71 @@ fun MyBasketView() {
             onDismiss = triggerBottomSheetModal,
             productsDataFlow = viewModel.productsDatabase,
             loadMoreObjects = { searchText, onLoadCompleted ->
-                viewModel.fetchMoreProducts(searchText, onLoadCompleted)
-            },
+                viewModel.fetchMoreProducts(searchText, onLoadCompleted) },
             onListUpdate = { viewModel.fetchProductsDatabase(it) },
             onSave = { viewModel.addIngredient(it) }
         )
-    }
 
+    }
     Column(
-        modifier = Modifier.background(color = Color.White).fillMaxSize().padding(top = 60.dp),
+        modifier = Modifier
+            .background(color = Color.White)
+            .fillMaxSize()
+            .padding(top = 60.dp),
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         Column(
             modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    "My Basket",
-                    fontSize = 25.sp,
-                    fontFamily = Montserrat,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.Black,
-                    style = TextStyle(letterSpacing = -1.sp)
-                )
-                Button(
-                    shape = RectangleShape,
+                Row(
                     modifier = Modifier
-                        .border(
-                            1.dp, Color(0xFFF5F5F5), shape = RoundedCornerShape(22.dp)
-                        )
-                        .clip(RoundedCornerShape(22.dp)).width(50.dp).height(50.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(0xFFF5F5F5),
-                        contentColor = Color.White
-                    ),
-                    contentPadding = PaddingValues(5.dp),
-                    onClick = { deleteSelected = true }
+                        .fillMaxWidth()
+                        .padding(start = 20.dp, end = 20.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.delete_bin_5_line__2_),
-                        contentDescription = "Back",
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier.width(20.dp).height(20.dp)
+                    Text(
+                        text = stringResource(id = R.string.my_basket),
+                        fontSize = 25.sp,
+                        fontFamily = Montserrat,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        style = TextStyle(letterSpacing = -1.sp)
                     )
+                    Button(
+                        shape = RectangleShape,
+                        modifier = Modifier
+                            .border(
+                                1.dp, Color(0xFFF5F5F5), shape = RoundedCornerShape(22.dp)
+                            )
+                            .clip(RoundedCornerShape(22.dp))
+                            .width(50.dp)
+                            .height(50.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFFF5F5F5),
+                            contentColor = Color.White
+                        ),
+                        contentPadding = PaddingValues(5.dp),
+                        onClick = { deleteSelected = true }
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.delete_bin_5_line__2_),
+                            contentDescription = stringResource(id = R.string.go_back),
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.width(20.dp).height(20.dp)
+                        )
+                    }
                 }
-            }
             Row(
-                modifier = Modifier.fillMaxWidth()
-                    .padding(end = 20.dp, start = 20.dp, bottom = 5.dp).height(80.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 20.dp, start = 20.dp, bottom = 5.dp)
+                    .height(80.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Spacer(modifier = Modifier.weight(1f))
+
                 Button(
                     shape = RectangleShape,
                     modifier = Modifier
@@ -163,57 +164,38 @@ fun MyBasketView() {
                     onClick = { triggerBottomSheetModal() }
                 ) {
                     Text(
-                        "Add items +",
+                        text = stringResource(id = R.string.add_items_plus),
                         fontSize = 13.sp,
                         fontFamily = Montserrat,
                         color = Color(126, 198, 11),
                     )
                 }
             }
-
-            //val productsList: List<Ingredient> by viewModel.productsList.collectAsState()
-            val updatedProductList = productsList.map { product ->
-                val isSelectedProduct = selectedIngredients.any { it.id == product.id }
-                product.copy(isSelected = isSelectedProduct)
-            }
-
-
-            if (updatedProductList.isNotEmpty()) {
-                LazyColumn(
-                    modifier = Modifier.padding(end = 20.dp, start = 20.dp, bottom = 110.dp)
-                ) {
-                    itemsIndexed(
-                        items = updatedProductList,
-                        key = { index, product -> "${product.id}_${index}" }
-                    ) { index, product ->
-
-                    }
-
-                    itemsIndexed(
-                        items = productsList,
-                        key = { index, ingredient -> "${index}_${ingredient.id}" }
-                    ) { index, ingredient ->
-                        val product = ingredient
-                        BasketIngredient(
-                            ingredient = product,
-                            isShown = !selectedProductsIds.contains(product.id) || !deleteSelected,
-                            onSelectionChange = { bool ->
-                                if (bool) viewModel.selectIngredient(product.id)
-                                else viewModel.unselectIngredient(product.id)
-                            },
-                            onIngredientUpdate = { viewModel.saveBasket() }
-                        )
-                    }
+            LazyColumn (modifier = Modifier.padding(end = 20.dp, start = 20.dp, bottom = 110.dp)) {
+                items(
+                    items = productsList.value,
+                    key = { ingredient -> ingredient.id }
+                ) { ingredient ->
+                    val product = ingredient
+                    BasketIngredient(
+                        ingredient = product,
+                        isShown = !selectedProductsIds.value.contains(product.id)||!deleteSelected,
+                        onSelectionChange = {bool ->
+                            if(bool) viewModel.selectIngredient(product.id)
+                            else viewModel.unselectIngredient(product.id) },
+                        onIngredientUpdate = { viewModel.saveBasket() }
+                    )
                 }
             }
+        }
 
-            LaunchedEffect(deleteSelected) {
-                if (deleteSelected) {
-                    delay(800)
-                    viewModel.deleteSelectedIngredients()
-                    deleteSelected = false
-                }
-            }
+    }
+
+    LaunchedEffect(deleteSelected) {
+        if(deleteSelected) {
+            delay(800)
+            viewModel.deleteSelectedIngredients()
+            deleteSelected = false
         }
     }
 }
@@ -222,14 +204,14 @@ fun MyBasketView() {
 fun BasketIngredient(ingredient: Ingredient, isShown: Boolean,
                      onSelectionChange: (isSelected: Boolean) -> Unit,
                      onIngredientUpdate: () -> Unit) {
-    var isSelected by remember { mutableStateOf(ingredient.isSelected) }
+    var isSelected by remember { mutableStateOf(false) }
 
     var quantity by remember { mutableStateOf(ingredient.quantity) }
     val type by remember { mutableStateOf(ingredient.type) }
     val unit by remember { mutableStateOf(ingredient.unit) }
 
     var showItem by remember { mutableStateOf(true) }
-    if (!isShown) {
+    if(!isShown) {
         showItem = false
     }
 
@@ -281,7 +263,7 @@ fun BasketIngredient(ingredient: Ingredient, isShown: Boolean,
                         Row(verticalAlignment = Alignment.CenterVertically) {
                             Image(
                                 painter = painterResource(id = R.drawable.baseline_arrow_left_24),
-                                contentDescription = "Profile Image",
+                                contentDescription = stringResource(id = R.string.profile_picture),
                                 modifier = Modifier.size(50.dp).padding(end = 15.dp)
                                     .clip(RoundedCornerShape(20.dp))
                                     .clickable {
@@ -298,7 +280,7 @@ fun BasketIngredient(ingredient: Ingredient, isShown: Boolean,
                             )
                             Image(
                                 painter = painterResource(id = R.drawable.baseline_arrow_right_24),
-                                contentDescription = "Profile Image",
+                                contentDescription = stringResource(id = R.string.profile_picture),
                                 modifier = Modifier.size(50.dp).padding(start = 15.dp)
                                     .clip(RoundedCornerShape(20.dp))
                                     .clickable {
