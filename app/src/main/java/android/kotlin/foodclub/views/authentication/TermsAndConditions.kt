f4516a69dd0c8864592cd385f48ae6b0256ee07b
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2023-12-19 12:13:10 +0100

    Fix merge conflicts

--- app/src/main/java/android/kotlin/foodclub/views/authentication/TermsAndConditions.kt
+++ app/src/main/java/android/kotlin/foodclub/views/authentication/TermsAndConditions.kt
@@ -1,13 +1,11 @@
 package android.kotlin.foodclub.views.authentication
 
-import android.annotation.SuppressLint
 import android.kotlin.foodclub.R
 import android.kotlin.foodclub.config.ui.Montserrat
 import android.kotlin.foodclub.config.ui.disabledContainerColor
 import android.kotlin.foodclub.config.ui.foodClubGreen
 import android.kotlin.foodclub.viewModels.authentication.termsAndConditions.TermsAndConditionsEvents
 import android.kotlin.foodclub.viewModels.authentication.termsAndConditions.TermsAndConditionsViewModel
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -18,17 +16,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -36,32 +30,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.rizzi.bouquet.ResourceType
-import com.rizzi.bouquet.VerticalPDFReader
-import com.rizzi.bouquet.rememberVerticalPdfReaderState
 
-@SuppressLint("UnrememberedMutableInteractionSource", "StateFlowValueCalledInComposition")
 @Composable
 fun TermsAndConditions(
     navController: NavHostController,
@@ -425,6 +408,6 @@ fun TermsAndConditions(
 fun TermsAndConditionsPreview() {
     val navController = rememberNavController()
     val viewModel = remember { TermsAndConditionsViewModel() }
-    TermsAndConditions(navController = navController, viewModel = viewModel)
+    TermsAndConditions(navController = navController, events = viewModel)
 }
 
