8110eb3af8ab90d2ed939492097be68dc1baf1cb
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2023-12-22 15:16:42 +0100

    Update Categories section and refactor code

--- app/src/main/java/android/kotlin/foodclub/domain/models/recipes/Category.kt
+++ app/src/main/java/android/kotlin/foodclub/domain/models/recipes/Category.kt
@@ -4,3 +4,15 @@ data class Category(
     val id: Int,
     val name: String
 )
+
+val allCategories = listOf(
+    Category(1, "Meat"),
+    Category(2, "Keto"),
+    Category(3, "High-protein"),
+    Category(4, "Vegan"),
+    Category(5, "Low-fat"),
+    Category(6, "Fat-reduction"),
+    Category(7, "Italian"),
+    Category(8, "Chinese"),
+    Category(9, "Vegetarian")
+)
