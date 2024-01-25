fb6c605f49a71a6f8b6152bbac68d54d961dfd7d
Orhan Uckulac <orhanuckulac0@gmail.com>
2024-01-25 16:48:25 +0300

    fixing discover view open post crash

---
+++ app/src/main/java/android/kotlin/foodclub/views/home/discover/DiscoverViewPosts.kt
@@ -0,0 +1,196 @@
+package android.kotlin.foodclub.views.home.discover
+
+import android.kotlin.foodclub.R
+import android.kotlin.foodclub.domain.models.home.VideoModel
+import android.kotlin.foodclub.domain.models.others.AnimatedIcon
+import android.kotlin.foodclub.domain.models.profile.SimpleUserModel
+import android.kotlin.foodclub.utils.composables.BackButton
+import android.kotlin.foodclub.utils.composables.LikeButton
+import android.kotlin.foodclub.utils.composables.PlayPauseButton
+import android.kotlin.foodclub.utils.composables.VideoLayout
+import android.kotlin.foodclub.utils.composables.VideoScroller
+import android.kotlin.foodclub.viewModels.home.discover.DiscoverEvents
+import android.kotlin.foodclub.viewModels.home.discover.DiscoverViewModel
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.PagerDefaults
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun DiscoverViewPosts(
+    viewModel: DiscoverViewModel = hiltViewModel(),
+    postId: Long,
+    posts: List<VideoModel>,
+    events: DiscoverEvents,
+    state: DiscoverState,
+    onBackPressed: () -> Unit
+){
+    val coroutineScope = rememberCoroutineScope()
+    val localDensity = LocalDensity.current
+    var screenHeightMinusBottomNavItem = LocalConfiguration.current.screenHeightDp.dp * 0.94f
+    if (screenHeightMinusBottomNavItem <= dimensionResource(id = R.dimen.dim_650)) {
+        screenHeightMinusBottomNavItem = LocalConfiguration.current.screenHeightDp.dp * 0.96f
+    }
+    val systemUiController = rememberSystemUiController()
+
+    var showIngredientSheet by remember { mutableStateOf(false) }
+    val triggerIngredientBottomSheetModal: () -> Unit = {
+        showIngredientSheet = !showIngredientSheet
+    }
+
+    val initialVideo = posts.filter { it.videoId == postId }.getOrNull(0)
+    val hasVideoLoaded = remember { mutableStateOf(false) }
+
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent,
+            darkIcons = false
+        )
+        systemUiController.setNavigationBarColor(
+            color = Color.Black
+        )
+    }
+    BackHandler {
+        onBackPressed()
+    }
+
+
+    Column(
+        modifier = Modifier
+            .height(screenHeightMinusBottomNavItem)
+    ) {
+        val currentPostIndex = posts.indexOf(initialVideo)
+        val pagerState = rememberPagerState(
+            initialPage = if (currentPostIndex == -1) 0 else currentPostIndex,
+            initialPageOffsetFraction = 0f
+        ){
+            posts.size
+        }
+
+        if (initialVideo != null) {
+            val fling = PagerDefaults.flingBehavior(
+                state = pagerState, lowVelocityAnimationSpec = tween(
+                    easing = LinearEasing, durationMillis = 300
+                )
+            )
+            VerticalPager(
+                state = pagerState,
+                flingBehavior = fling,
+                beyondBoundsPageCount = 1,
+                modifier = Modifier
+            ) { vtPager ->
+                var pauseButtonVisibility by remember { mutableStateOf(false) }
+                val doubleTapState by remember {
+                    mutableStateOf(
+                        AnimatedIcon(R.drawable.liked, 110.dp, localDensity)
+                    )
+                }
+                val currentVideo = posts[vtPager]
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    var isLiked by remember {
+                        mutableStateOf(currentVideo.currentViewerInteraction.isLiked)
+                    }
+                    var isBookmarked by remember {
+                        mutableStateOf(
+                            currentVideo.currentViewerInteraction.isBookmarked
+                        )
+                    }
+                    VideoScroller(
+                        viewModel.exoPlayer,
+                        currentVideo,
+                        pagerState,
+                        vtPager,
+                        onSingleTap = {
+                            pauseButtonVisibility = it.isPlaying
+                            it.playWhenReady = !it.isPlaying
+                        },
+                        onDoubleTap = { exoPlayer, offset ->
+                            coroutineScope.launch {
+                                doubleTapState.animate(offset)
+                            }
+                        },
+                        onVideoDispose = {
+                            pauseButtonVisibility = false
+                        },
+                        onVideoGoBackground = { pauseButtonVisibility = false }
+                    )
+
+                    hasVideoLoaded.value = true
+
+                    LikeButton(doubleTapState) {}
+
+                    val simpleUserModel = SimpleUserModel(
+                        userId = state.userId.toInt(),
+                        username = currentVideo.authorDetails,
+                        profilePictureUrl = null
+                    )
+                    PlayPauseButton(buttonVisibility = pauseButtonVisibility)
+
+                    VideoLayout(
+                        userDetails = simpleUserModel,
+                        videoStats = currentVideo.videoStats,
+                        likeState = isLiked,
+                        bookMarkState = isBookmarked,
+                        category = stringResource(id = R.string.meat),
+                        opacity = 0.7f,
+                        onLikeClick = {
+                            isLiked = !isLiked
+                            coroutineScope.launch {
+                                // TODO like functionality
+                            }
+                        },
+                        onBookmarkClick = {
+                            isBookmarked = !isBookmarked
+                            coroutineScope.launch {
+                                // TODO bookmark functionality
+                            }
+                        },
+                        onInfoClick = triggerIngredientBottomSheetModal,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.BottomCenter)
+                    )
+                    Box(modifier = Modifier
+                        .padding(
+                            top = dimensionResource(id = R.dimen.dim_40),
+                            start = dimensionResource(id = R.dimen.dim_10)
+                        )
+                    ) {
+                        BackButton(onBackPressed, backgroundTransparent = true, Color.White)
+                    }
+                }
+            }
+        }
+    }
+}
