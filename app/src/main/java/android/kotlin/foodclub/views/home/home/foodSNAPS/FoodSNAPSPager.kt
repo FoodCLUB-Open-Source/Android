506fe054034b4fbb12a2290031e00d160abd9b82
Ky Vu <vuky9999@gmail.com>
2024-02-14 01:47:30 +0700

    add swipe down to display memories reel by using pull refresh

--- app/src/main/java/android/kotlin/foodclub/views/home/home/foodSNAPS/FoodSNAPSPager.kt
+++ app/src/main/java/android/kotlin/foodclub/views/home/home/foodSNAPS/FoodSNAPSPager.kt
@@ -22,6 +22,9 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.SwipeableState
+import androidx.compose.material.pullrefresh.PullRefreshState
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material.rememberSwipeableState
 import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
@@ -37,6 +40,7 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.navigation.NavHostController
 import kotlinx.coroutines.launch
 import okio.ByteString.Companion.encodeUtf8
+import java.nio.file.WatchEvent
 import kotlin.math.roundToInt
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
@@ -54,10 +58,22 @@ fun FoodSNAPSPager(
 ) {
     var isReactionsClickable by remember { mutableStateOf(selectedReaction != Reactions.ALL) }
 
-    var check by remember{
-        mutableStateOf(true)
+    //Swipe down to display the memories reel by using pull to refresh
+    val refreshScope = rememberCoroutineScope()
+    var refreshing by remember { mutableStateOf(false) }
+
+    fun refresh() = refreshScope.launch {
+        refreshing = true
+        changeMemoriesReelVisibility(true)
+        refreshing = false
     }
 
+    var inSnapView = rememberPullRefreshState(
+        refreshing = refreshing,
+        onRefresh = ::refresh
+    )
+
+    // animation variables for vertical pager
     val ANIMATION_DURATION_SHORT = 300
     val snapPagerFling = PagerDefaults.flingBehavior(
         state = snapPagerState,
@@ -76,12 +92,7 @@ fun FoodSNAPSPager(
                 swipeableState.snapTo(SwipeDirection.NEUTRAL)
             }
 
-            SwipeDirection.DOWN -> {
-                if (!showMemoriesReel) {
-                    changeMemoriesReelVisibility(true)
-                }
-                swipeableState.snapTo(SwipeDirection.NEUTRAL)
-            }
+            SwipeDirection.DOWN -> {}
 
             SwipeDirection.NEUTRAL -> {}
         }
@@ -97,47 +108,28 @@ fun FoodSNAPSPager(
                 .aspectRatio(0.9f, true)
         )
     } else {
-        HorizontalPager(
+        VerticalPager(
             state = snapPagerState,
             flingBehavior = snapPagerFling,
             beyondBoundsPageCount = 1,
-            modifier = Modifier
+            modifier = Modifier.pullRefresh(inSnapView)
         ) {
             ReactionsOverlay(
                 modifier = Modifier
                     .then(
-//                        if (it == 0) {
+                        if (it == 0 && showMemoriesReel) {
                             Modifier.swipeable(
                                 state = swipeableState,
                                 anchors = mapOf(
                                     0f to SwipeDirection.NEUTRAL,
-                                    -1f to SwipeDirection.UP,
-                                    1f to SwipeDirection.DOWN
+                                    -1f to SwipeDirection.UP
                                 ),
                                 thresholds = { _, _ -> FractionalThreshold(0.3f) },
                                 orientation = Orientation.Vertical,
                             )
-//                            if (showMemoriesReel){
-//                                Modifier.swipeable(
-//                                    state = swipeableState,
-//                                    anchors = mapOf(
-//                                        0f to SwipeDirection.NEUTRAL,
-//                                        -1f to SwipeDirection.UP
-//                                    ),
-//                                    thresholds = { _, _ -> FractionalThreshold(0.3f) },
-//                                    orientation = Orientation.Vertical,
-//                                )
-//                            } else {
-//                                Modifier
-//                                    .pointerInput(Unit) {
-//                                        detectVerticalDragGestures { _, dragAmount ->
-//                                            Log.i("111111", dragAmount.toString())
-//                                        }
-//                                    }
-//                            }
-//                        } else {
-//                            Modifier
-//                        }
+                        } else {
+                            Modifier
+                        }
                     ),
                 selectedReaction = selectedReaction,
                 clearSelectedReaction = clearSelectedReactions,
