5fd2cb8295cc1107b2872da511213934391606ed
Ky Vu <vuky9999@gmail.com>
2024-02-24 18:55:06 +0700

    update arguments and add composable instruction

--- app/src/main/java/android/kotlin/foodclub/utils/composables/PermissionErrorBox.kt
+++ app/src/main/java/android/kotlin/foodclub/utils/composables/PermissionErrorBox.kt
@@ -2,8 +2,11 @@ package android.kotlin.foodclub.utils.composables
 
 
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.kotlin.foodclub.R
+import android.net.Uri
+import android.provider.Settings
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
@@ -26,19 +29,31 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
 
-
+/**
+ * PermissionErrorBox composable will ask the user for permission before run content
+ *  Input:
+ *      @permissions: is an array of permissions string
+ *      @title: title of the dialog
+ *      @rational: reason for the user why we need these permissions
+ *      @navController: when user click go back it will return back to previous screen
+ *      @context: LocalContext.current
+ *      @content: Composable content will work if permissions are allowed
+ *
+ *  Output:
+ *      Request permissions if the permissions are not allowed then
+ *      diplay a dialog to ask the user go to setting to allow permissions
+ **/
 @Composable
-fun CameraPermissionErrorBox(
+fun PermissionErrorBox(
     permissions: Array<String>,
     title: String,
     rational: String,
-    onDismissRequest: () -> Unit,
-    onConfirmRequest: () -> Unit,
+    navController: NavController,
     context: Context,
     content: @Composable () -> Unit
 ) {
-
     var showDialog by remember {
         mutableStateOf(false)
     }
@@ -83,8 +98,15 @@ fun CameraPermissionErrorBox(
         visibleState = showDialog,
         title = title,
         text = rational,
-        onDismissRequest = { onDismissRequest() },
-        onConfirmRequest = { onConfirmRequest() }
+        onDismissRequest = { navController.popBackStack() },
+        onConfirmRequest = {
+            val i = Intent(
+                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.fromParts("package", context.packageName, null)
+            )
+            context.startActivity(i)
+            navController.popBackStack()
+        }
     )
 
     if (!showDialog) {
