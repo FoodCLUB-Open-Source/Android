aa8663f8a95da2a9cbcc2e55cd58fcc216e8dd33
Orhan Uckulac <orhanuckulac0@gmail.com>
2024-02-28 03:19:56 +0300

    unfinished there is something triggering double recomposition

--- app/src/main/java/android/kotlin/foodclub/views/home/home/foodSNAPS/ReactionsOverlay.kt
+++ app/src/main/java/android/kotlin/foodclub/views/home/home/foodSNAPS/ReactionsOverlay.kt
@@ -1,37 +1,34 @@
 package android.kotlin.foodclub.views.home.home.foodSNAPS
 
-import android.kotlin.foodclub.R
 import android.kotlin.foodclub.domain.enums.Reactions
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExitTransition
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.MutableTransitionState
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.animateIntAsState
-import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
+import androidx.compose.animation.core.VectorConverter
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.core.updateTransition
-import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 @Composable
@@ -43,6 +40,32 @@ fun ReactionsOverlay(
     content: @Composable () -> Unit,
 ) {
     var visibility by remember { mutableStateOf(selectedReaction != Reactions.ALL) }
+    val configuration = LocalConfiguration.current
+    val targets = IntOffset(
+        x = Random.nextInt(0 , configuration.screenWidthDp),
+        y = configuration.screenHeightDp
+    )
+
+    val scope = rememberCoroutineScope()
+    val particlesList = remember { mutableStateListOf<Particle>() }
+
+    LaunchedEffect(selectedReaction){
+        if (selectedReaction == Reactions.ALL) return@LaunchedEffect
+
+        for (i in 1..PARTICLE_QUANTITY){
+            scope.launch {
+                delay((i * DELAY_BETWEEN_PARTICLES).toLong())
+                particlesList.add(
+                    Particle(
+                        i,
+                        offsetX = Random.nextInt(0, configuration.screenWidthDp),
+                        offsetY = Random.nextInt(0, (configuration.screenHeightDp * 0.1).toInt()),
+                        selectedReaction
+                    )
+                )
+            }
+        }
+    }
 
     LaunchedEffect(key1 = visibility) {
         if (!visibility) {
@@ -50,177 +73,123 @@ fun ReactionsOverlay(
             return@LaunchedEffect
         }
         isReactionsClickable(false)
-        delay(MAX_ANIMATION_DURATION.toLong() + DELAY_MILLIS)
         visibility = false
         clearSelectedReaction()
         isReactionsClickable(true)
     }
-
     LaunchedEffect(key1 = selectedReaction) {
         if (selectedReaction == Reactions.ALL) return@LaunchedEffect
         visibility = true
     }
 
-    Box(modifier = modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
         content()
 
-        AnimatedVisibility(
-            visible = visibility,
-            enter = slideInVertically(
-                initialOffsetY = { it },
-                animationSpec = tween(
-                    durationMillis = MAX_ANIMATION_DURATION.toInt() - DELAY_MILLIS,
-                    easing = LinearEasing,
-                    delayMillis = DELAY_MILLIS
-                )
-            ),
-            exit = ExitTransition.None
-        ) {
-            val particles = remember { calculateParticleParams(reaction = selectedReaction) }
+        MainUI(
+            particlesList,
+            targets
+        ) { particleToRemove ->
+            particlesList.filter { currentParticle ->
+                currentParticle.id != particleToRemove.id
+            }
+        }
+    }
+}
+@Composable
+fun MainUI(
+    particlesList: List<Particle>,
+    targets: IntOffset,
+    onAnimationFinished: (Particle) -> Unit
+) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if (particlesList.isNotEmpty()){
+            particlesList.forEach { particle ->
+                key(particle.id) {
+                    val opacityAnimatable = remember { Animatable(0f) }
+                    val offsetXAnimatable = remember { Animatable(targets.x, Int.VectorConverter) }
+                    val offsetYAnimatable = remember { Animatable(targets.y, Int.VectorConverter) }
 
-            val height by animateIntAsState(
-                targetValue = MIN_HEIGHT,
-                animationSpec = tween(
-                    durationMillis = MAX_ANIMATION_DURATION.toInt(),
-                    easing = LinearOutSlowInEasing
-                ), label = stringResource(id = R.string.height_transition)
-            )
-
-            Layout(
-                modifier = modifier.padding(bottom = dimensionResource(id = R.dimen.reactions_overlay_bottom_padding)),
-                content = {
-                    for (i in 0 until PARTICLE_QUANTITY) {
-                        Particle(particles[i])
-                    }
-                }
-            ) { measurables, constraints ->
-                val placeables = measurables.map { it.measure(constraints) }
-                layout(constraints.maxWidth, height) {
-                    placeables.forEachIndexed { index, placeable ->
-                        val params = particles[index]
-                        placeable.placeRelative(
-                            x = particleXPosition(params.horizontalFraction, constraints.maxWidth),
-                            y = particleYPosition(params.verticalFraction, height)
-                        )
+                    SingleReactionContainer(
+                        particle,
+                        opacityAnimatable,
+                        offsetXAnimatable,
+                        offsetYAnimatable
+                    ) {
+                        onAnimationFinished(it)
                     }
                 }
             }
         }
     }
 }
-
-private fun calculateParticleParams(
-    reaction: Reactions
-): List<ParticleModel> {
-    val random = Random(System.currentTimeMillis().toInt())
-    val result = mutableListOf<ParticleModel>()
-    for (i in 0 until PARTICLE_QUANTITY) {
-        val verticalFraction = random.nextDouble(from = 0.0, until = 1.0).toFloat()
-        val horizontalFraction = random.nextDouble(from = 0.0, until = 1.0).toFloat()
-
-        val model =
-            ParticleModel(
-                verticalFraction = verticalFraction,
-                horizontalFraction = horizontalFraction,
-                initialScale = lerp(MIN_PARTICLE_SIZE, MAX_PARTICLE_SIZE, verticalFraction),
-                duration = lerp(
-                    MIN_ANIMATION_DURATION,
-                    MAX_ANIMATION_DURATION,
-                    verticalFraction
-                ).toInt(),
-                reaction = reaction
-            )
-        result.add(
-            model
-        )
-    }
-    return result
-}
-
-private fun lerp(start: Float, stop: Float, fraction: Float) =
-    (start * (1 - fraction) + stop * fraction)
-
-private fun particleXPosition(
-    horizontalFraction: Float,
-    maxWidth: Int
-) = ((horizontalFraction * maxWidth).toInt() - (maxWidth / PARTICLE_X_MULTIPLIER)).coerceIn(
-    minimumValue = PARTICLE_X_OFFSET,
-    maximumValue = maxWidth - PARTICLE_X_OFFSET
-)
-
-private fun particleYPosition(
-    verticalFraction: Float,
-    height: Int
-) = ((verticalFraction * height).toInt() + height / PARTICLE_Y_MULTIPLIER)
 
 @Composable
-private fun Particle(model: ParticleModel) {
-    if (model.reaction == Reactions.ALL) return
-
-    val transitionState = remember {
-        MutableTransitionState(0.1f).apply {
-            targetState = 0f
-        }
+fun SingleReactionContainer(
+    item: Particle,
+    opacityAnimatable: Animatable<Float, AnimationVector1D>,
+    offsetXAnimatable: Animatable<Int, AnimationVector1D>,
+    offsetYAnimatable: Animatable<Int, AnimationVector1D>,
+    onAnimationFinished: (Particle) -> Unit
+) {
+    LaunchedEffect(item) {
+        val opacity =
+            async {
+                opacityAnimatable.animateTo(1f, animationSpec = tween(OPACITY))
+            }
+        val offsetY =
+            async {
+                offsetYAnimatable.animateTo(
+                    targetValue = item.offsetY - OFFSET_ABOVE_HEIGHT,
+                    animationSpec = tween(PARTICLE_Y_SPEED)
+                )
+            }
+        val offsetX =
+            async {
+                offsetXAnimatable.animateTo(
+                    targetValue = item.offsetX,
+                    animationSpec = tween(PARTICLE_X_SPEED)
+                )
+            }
+        awaitAll(offsetX, offsetY, opacity)
+        opacityAnimatable.animateTo(0f, animationSpec = tween(OPACITY_SPEED))
+        onAnimationFinished(item)
     }
 
-    val transition = updateTransition(
-        transitionState,
-        label = stringResource(id = R.string.particle_transition)
-    )
-
-    val alpha by transition.animateFloat(
-        transitionSpec = {
-            keyframes {
-                durationMillis = model.duration
-                0.1f at START_OF_ANIMATION
-                1f at (model.duration * 0.1f).toInt()
-                1f at (model.duration * 0.8f).toInt()
-                0f at model.duration
-            }
-        }, label = ""
-    ) { it }
-
-    val targetScale = model.initialScale * TARGET_PARTICLE_SCALE_MULTIPLIER
-    val scale by transition.animateFloat(
-        transitionSpec = {
-            keyframes {
-                durationMillis = model.duration
-                model.initialScale at START_OF_ANIMATION
-                model.initialScale at (model.duration * 0.7f).toInt()
-                targetScale at model.duration
-            }
-        }, label = ""
-    ) { it }
-
-    Image(
+    Box(
         modifier = Modifier
-            .size(PARTICLE_SIZE.dp)
-            .scale(scale)
-            .alpha(alpha),
-        painter = painterResource(id = model.reaction.drawable),
-        contentDescription = ""
-    )
+            .fillMaxSize()
+            .offset {
+                IntOffset(
+                    x = offsetXAnimatable.value.dp.roundToPx(),
+                    y = offsetYAnimatable.value.dp.roundToPx()
+                )
+            }
+            .graphicsLayer {
+                alpha = opacityAnimatable.value
+            }
+    ) {
+        Image(
+            painter = painterResource(id = item.reactions.drawable),
+            contentDescription = "",
+            Modifier.size(40.dp)
+        )
+    }
 }
 
-private const val TARGET_PARTICLE_SCALE_MULTIPLIER = 1.3f
-private const val START_OF_ANIMATION = 0
-private const val MIN_PARTICLE_SIZE = 1f
-private const val MAX_PARTICLE_SIZE = 2.6f
-private const val MIN_ANIMATION_DURATION = 1200f
-private const val MAX_ANIMATION_DURATION = 1500f
-private const val PARTICLE_SIZE = 20
-private const val MIN_HEIGHT = 800 // larger than max as its displacement from top of screen
-private const val MAX_HEIGHT = 300
-private const val PARTICLE_QUANTITY = 10
-private const val PARTICLE_X_OFFSET = 50
-private const val PARTICLE_X_MULTIPLIER = 10
-private const val PARTICLE_Y_MULTIPLIER = 10
-private const val DELAY_MILLIS = 0
+private const val PARTICLE_QUANTITY = 5
+private const val PARTICLE_Y_SPEED = 6000
+private const val PARTICLE_X_SPEED = 6000
+private const val OPACITY = 500
+private const val OPACITY_SPEED = 2000
+private const val OFFSET_ABOVE_HEIGHT = 500
+private const val X_OFFSET_REDUCTION = 500
+private const val DELAY_BETWEEN_PARTICLES = 100
 
-data class ParticleModel(
-    val verticalFraction: Float,
-    val horizontalFraction: Float,
-    val initialScale: Float,
-    val duration: Int,
-    val reaction: Reactions
+data class Particle(
+    val id: Int,
+    val offsetX: Int,
+    val offsetY: Int,
+    val reactions: Reactions
 )
