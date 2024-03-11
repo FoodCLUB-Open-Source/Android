4f7764a60cda87f2faf18f6e5ae373b0ecc184c3
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2024-03-11 17:45:29 +0000

    Fix authenticator

--- app/src/main/java/android/kotlin/foodclub/network/retrofit/utils/auth/ResponsesInterceptor.kt
+++ app/src/main/java/android/kotlin/foodclub/network/retrofit/utils/auth/ResponsesInterceptor.kt
@@ -1,17 +1,28 @@
 package android.kotlin.foodclub.network.retrofit.utils.auth
 
-import android.util.Log
+import android.kotlin.foodclub.network.retrofit.responses.general.SingleMessageResponse
+import com.google.gson.Gson
 import okhttp3.Interceptor
 import okhttp3.Response
+import java.util.zip.GZIPInputStream
 
 class ResponsesInterceptor: Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalResponse = chain.proceed(chain.request())
-        val responseBody = originalResponse.peekBody(Long.MAX_VALUE)
-        Log.d("Interceptor", responseBody.string())
+        val responseBody = try {
+            val responseText = GZIPInputStream(
+                originalResponse.peekBody(Long.MAX_VALUE).byteStream()
+            ).bufferedReader(Charsets.UTF_8).use { it.readText() }
+            Gson().fromJson(responseText, SingleMessageResponse::class.java)
+        } catch (e: Exception) {
+            SingleMessageResponse(message = "${originalResponse.code} ${originalResponse.message}")
+        }
+
         if (originalResponse.code == 404
-            && responseBody.string().contains("Token expired")) {
-            return originalResponse.newBuilder().code(401).build()
+            && responseBody?.message?.contains("Token expired") == true) {
+            val newRequest = originalResponse.request
+                .newBuilder().removeHeader("Authorisation").build()
+            return originalResponse.newBuilder().request(newRequest).code(401).build()
         }
         return originalResponse
     }
