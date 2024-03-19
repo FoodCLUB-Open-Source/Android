baf12f9b6416f40e09aef2c50aef084bddc48515
Ky Vu <vuky9999@gmail.com>
2024-03-20 05:35:29 +1100

    add Category Type, diet for categories section and cuisine for world section

--- app/src/main/java/android/kotlin/foodclub/domain/enums/Category.kt
+++ app/src/main/java/android/kotlin/foodclub/domain/enums/Category.kt
@@ -1,15 +1,15 @@
 package android.kotlin.foodclub.domain.enums
 
-enum class Category(val displayName: String) {
-    MEAT("Meat"),
-    KETO("Keto"),
-    HIGH_PROTEIN("High-protein"),
-    VEGAN("Vegan"),
-    LOW_FAT("Low-fat"),
-    FAT_REDUCTION("Fat-reduction"),
-    ITALIAN("Italian"),
-    CHINESE("Chinese"),
-    VEGETARIAN("Vegetarian");
+enum class Category(val displayName: String, val categoryType: CategoryType) {
+    MEAT("Meat", CategoryType.DIET),
+    KETO("Keto", CategoryType.DIET),
+    HIGH_PROTEIN("High-protein", CategoryType.DIET),
+    VEGAN("Vegan", CategoryType.DIET),
+    LOW_FAT("Low-fat", CategoryType.DIET),
+    FAT_REDUCTION("Fat-reduction", CategoryType.DIET),
+    ITALIAN("Italian", CategoryType.CUISINE),
+    CHINESE("Chinese", CategoryType.CUISINE),
+    VEGETARIAN("Vegetarian", CategoryType.CUISINE);
 
     companion object {
         fun deriveFromName(name: String): Category? {
@@ -17,5 +17,18 @@ enum class Category(val displayName: String) {
             if(derivedCategories.isNotEmpty()) return derivedCategories[0]
             return null
         }
+
+        fun deriveFromType(categoryType: CategoryType): List<Category> {
+            val categoryList = entries.filter { it.categoryType == categoryType }
+
+            return categoryList.ifEmpty {
+                emptyList()
+            }
+        }
     }
+}
+
+enum class CategoryType{
+    DIET,
+    CUISINE
 }
