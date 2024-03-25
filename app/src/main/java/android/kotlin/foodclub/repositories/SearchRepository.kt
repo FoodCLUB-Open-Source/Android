52e48c4c5d20ac0b3fffe070c4b81c5a7bf257ab
Ky Vu <vuky9999@gmail.com>
2024-03-26 06:38:21 +1100

    create search repository

---
+++ app/src/main/java/android/kotlin/foodclub/repositories/SearchRepository.kt
@@ -0,0 +1,29 @@
+package android.kotlin.foodclub.repositories
+
+import android.kotlin.foodclub.network.retrofit.dtoModels.search.SearchDto
+import android.kotlin.foodclub.network.retrofit.responses.general.DefaultErrorResponse
+import android.kotlin.foodclub.network.retrofit.responses.search.SearchUserPostsResponse
+import android.kotlin.foodclub.network.retrofit.services.SearchService
+import android.kotlin.foodclub.utils.helpers.Resource
+
+class SearchRepository(
+    private val api: SearchService
+) {
+    suspend fun searchByText(
+        searchText: String?
+    ): Resource<SearchUserPostsResponse, DefaultErrorResponse>{
+        try {
+            val response = api.searchPosts(SearchDto(searchText))
+            if (response.isSuccessful) {
+                val responseBody = response.body()
+                if (responseBody != null) {
+                    return Resource.Success(responseBody)
+                }
+            }
+        } catch (e: Exception) {
+            return Resource.Error("Failed to search by Text: ${e.message}")
+        }
+
+        return Resource.Error("Failed to search by Text")
+    }
+}
