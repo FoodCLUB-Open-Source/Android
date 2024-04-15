8d9fccf9224146189c6aa4da87c110c74fc6628f
acb21jk <jkreczetowski1@sheffield.ac.uk>
2024-04-15 11:38:25 +0100

    Fix ResponsesInterceptor by checking if the response from the server is gzipped or not

--- app/src/main/java/android/kotlin/foodclub/network/retrofit/utils/auth/ResponsesInterceptor.kt
+++ app/src/main/java/android/kotlin/foodclub/network/retrofit/utils/auth/ResponsesInterceptor.kt
@@ -1,6 +1,7 @@
 package android.kotlin.foodclub.network.retrofit.utils.auth
 
 import android.kotlin.foodclub.network.retrofit.responses.general.SingleMessageResponse
+import android.util.Log
 import com.google.gson.Gson
 import okhttp3.Interceptor
 import okhttp3.Response
@@ -9,21 +10,34 @@ import java.util.zip.GZIPInputStream
 class ResponsesInterceptor: Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalResponse = chain.proceed(chain.request())
+
         val responseBody = try {
-            val responseText = GZIPInputStream(
-                originalResponse.peekBody(Long.MAX_VALUE).byteStream()
-            ).bufferedReader(Charsets.UTF_8).use { it.readText() }
+            val responseText = if(isGzipped(originalResponse)) {
+                GZIPInputStream(
+                    originalResponse.peekBody(Long.MAX_VALUE).byteStream()
+                ).bufferedReader(Charsets.UTF_8).use { it.readText() }
+            } else {
+                originalResponse.peekBody(Long.MAX_VALUE).string()
+            }
+
             Gson().fromJson(responseText, SingleMessageResponse::class.java)
         } catch (e: Exception) {
+            Log.d("ResponsesInterceptor", e.message.toString())
             SingleMessageResponse(message = "${originalResponse.code} ${originalResponse.message}")
         }
 
         if (originalResponse.code == 404
             && responseBody?.message?.contains("Token expired") == true) {
+            Log.d("ResponsesInterceptor", "holds")
             val newRequest = originalResponse.request
                 .newBuilder().removeHeader("Authorisation").build()
             return originalResponse.newBuilder().request(newRequest).code(401).build()
         }
         return originalResponse
+    }
+
+    private fun isGzipped(response: Response): Boolean {
+        return response.header("Content-Encoding") != null && response.header("Content-Encoding")
+            .equals("gzip")
     }
 }
