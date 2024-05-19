6e7662189bcba14f8f6b2f596d8d24ffd6a72c8c
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2024-05-19 23:57:57 +0100

    Remove unused converters

--- app/src/main/java/live/foodclub/localdatasource/room/converters/Converters.kt
+++ app/src/main/java/live/foodclub/localdatasource/room/converters/Converters.kt
@@ -1,7 +1,6 @@
 package live.foodclub.localdatasource.room.converters
 
 import androidx.room.TypeConverter
-import live.foodclub.domain.enums.PostType
 
 class Converters {
 
@@ -14,15 +13,5 @@ class Converters {
     fun toListString(data: String): List<String> {
         return listOf(*data.split(",").map { it.ifEmpty { "" } }
             .toTypedArray())
-    }
-
-    @TypeConverter
-    fun fromPostType(value: PostType): Int {
-        return value.ordinal
-    }
-
-    @TypeConverter
-    fun toPostType(value: Int): PostType {
-        return PostType.parsePostType(value)
     }
 }
