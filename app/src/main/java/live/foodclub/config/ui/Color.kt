4955d2fd663c212b7e228b3dca8dc8bfc2af8939
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2024-05-25 23:40:55 +0100

    Make changes to log out dialog

--- app/src/main/java/live/foodclub/config/ui/Color.kt
+++ app/src/main/java/live/foodclub/config/ui/Color.kt
@@ -15,6 +15,7 @@ val foodClubGreen = Color(0xFF7EC60B)
 val disabledContainerColor = Color(0xFFC9C9C9)
  val Black=Color(0xFF000000)
 val light_blue=Color(0xFF3A7CA8)
+val text_blue = Color(0xFF0A7AFF)
 
 val containerColor = Color(0xFFF5F5F5)
 val Orange =Color(0xFFFfa500)
