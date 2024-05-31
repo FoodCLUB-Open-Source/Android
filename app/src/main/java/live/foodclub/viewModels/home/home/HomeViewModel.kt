c2519e9900ea862723db41875f4e83f5e0fac1b5
Eric Leon <ericleon727@gmail.com>
2024-05-31 17:16:08 +0100

    Fixed SimpleUserModel and its endpoints, as well as any import conflicts

--- app/src/main/java/live/foodclub/viewModels/home/home/HomeViewModel.kt
+++ app/src/main/java/live/foodclub/viewModels/home/home/HomeViewModel.kt
@@ -1,11 +1,6 @@
 package live.foodclub.viewModels.home.home
 
-import android.util.Log
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.media3.exoplayer.ExoPlayer
-import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
